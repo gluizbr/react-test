824f7aee3178285aceb4e3affc34afb4288cb13b
gluizbr <bielraawr@gmail.com>
2018-10-26 11:57:34 -0300

    Updates

---
+++ precache-manifest.7a273b78c08ba358b55a70f72d41ceaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "8420e3288eee9a84d808",
+    "url": "/react-test/static/js/main.8420e328.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/react-test/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "8420e3288eee9a84d808",
+    "url": "/react-test/static/css/main.d9e87d94.chunk.css"
+  },
+  {
+    "revision": "ed58f762e30d6ad41de3e5799734ad82",
+    "url": "/react-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gluizbr/react-test/precache-manifest.5558f65f4b5cee9eebf0695945bc1f29.js"
+  "/react-test/precache-manifest.7a273b78c08ba358b55a70f72d41ceaf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gluizbr/react-test/index.html", {
+workbox.routing.registerNavigationRoute("/react-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
